323c82d3a5a93ce4a82411aeebad1378b6b51b22
Junhan93 <junhan.3008@gmail.com>
2021-07-20 00:16:24 +0800

    refactored and fixed getTotalPriceTier

--- app/Classes/PriceHelper.php
+++ app/Classes/PriceHelper.php
@@ -26,15 +26,18 @@ class PriceHelper
     public static function getUnitPriceTierAtQty(int $qty, array $tiers): float
     {
         $unitPrice = 0;
-        $tierKeys = array_keys($tiers);
-        $tierValues = array_values($tiers);
+        $tier      = array_keys($tiers);
+        $price     = array_values($tiers);
 
         if ( $qty > 0 ) {
             // looped thru all so we don't have to worry if someone changes the priceTiers
             for ($i=0; $i < count($tiers); $i++) {
-                // this prevents $i+1 to return something more than the amount of tiers 
-                if ( $i === count($tiers)-1 || $qty >= $tierKeys[$i] && $qty < $tierKeys[$i+1] ) {
-                    $unitPrice = $tierValues[$i];
+                $current = $tier[$i];
+                $next = next($tier);
+
+                // this prevents $i+1 to return something more than the maximum amount of tiers 
+                if ( $i === count($tiers)-1 || $qty >= $current && $qty < $next ) {
+                    $unitPrice = $price[$i];
                     return $unitPrice; 
                 }
             } 
@@ -56,26 +59,39 @@ class PriceHelper
      */
     public static function getTotalPriceTierAtQty(int $qty, array $tiers): float
     {
-        // get the maximum total price of each tier
-        $tier1max = (array_keys($tiers)[1] - 1) * array_values($tiers)[0]; // 15000 = 10000 * 1.5
-        $tier2max = (array_keys($tiers)[2] - 1) - (array_keys($tiers)[1] - 1) * array_values($tiers)[1]; // 90,000 = 100,000 - 10,000(max quantity for prev tier) * 1
-
-        if ( $qty <= 0 ) {
-            $price = 0;
+        $totalprice = 0;
+        $tier  = array_keys($tiers);
+        $price = array_values($tiers);
+        $last  = count($tiers)-1;
+        $maxTierPrice = [];
+        
+        // get maximum price of tiers below qty, get tier level of qty, add them tgt. 
+        for ( $i=0; $i < count($tiers); $i++ ) { 
+            $current = $tier[$i];
+            $next    = next($tier);
+            
+            // save max price of first tier to maxTierPrice[] 
+            if ( $i === 0 ) {
+                $maxTierPrice[] = ( $next-1 ) * $price[$i];
+            }
+    
+            // if iterating in middle, save the maximum price of the tiers to maxTierPrice[]  
+            if ( $i > 0 && $i < $last ) {
+                $maxTierPrice[] = (( $next-1 ) - ( $current-1 )) * $price[$i];
+            }
+    
+            // return total price for qty that only reach the first tier, more than tier 0 but lesser than tier 1
+            if ( $qty >= 0 && $qty < $tier[1] ) {
+                $totalprice = $qty * $price[0];
+                return $totalprice;
+            }
+    
+            // return total price for qty for rest of tiers, $i = last condition to prevent $i+1 to return more than tier count
+            if ( $i === $last || $qty >= $current && $qty < $next ) {
+                $totalprice = (( $qty - ($current-1) ) * $price[$i]) + array_sum(array_slice($maxTierPrice, 0, $i));
+                return $totalprice;
+            } 
         }
-        // return total price for qty more than 0 AND less than 10,001
-        else if ( $qty >= array_keys($tiers)[0] && $qty < array_keys($tiers)[1] ) {
-            $price = $qty * array_values($tiers)[0];
-        } 
-        // return total price for qty more than 10,001 AND less than 100,001
-        else if ( $qty >= array_keys($tiers)[1] && $qty < array_keys($tiers)[2] ) {
-            $price = ( ($qty - (array_keys($tiers)[1] - 1) ) * array_values($tiers)[1] ) + $tier1max;
-        }
-        // return total price for qty more than or equals to 100,001
-        else {
-            $price = ( ($qty - (array_keys($tiers)[2] - 1) ) * array_values($tiers)[2] ) + $tier1max + $tier2max;
-        }
-        return $price;
     }
 
     /**
